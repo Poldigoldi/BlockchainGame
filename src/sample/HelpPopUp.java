59118b78bce7e823ced314b805794c3d102adab5
Leonard Goldschmidt <leo.goldschmidt@yellowdog.cot>
2019-05-17 14:36:59 +0100

    helpPopUp fixes

--- src/sample/HelpPopUp.java
+++ src/sample/HelpPopUp.java
@@ -1,28 +1,16 @@
 package sample;
 
 import javafx.scene.control.TextField;
-public class HelpPopUp {
-    private String fullString, visibleString;
-    private double posX, posY;
+
+class HelpPopUp {
     private TextField popUp;
 
-    public void setPosX(double posX) {
-        this.posX = posX;
-    }
-
-    public void setPosY(double posY) {
-        this.posY = posY;
-    }
-
     HelpPopUp(String fullString, double posX, double posY) {
-        this.fullString = fullString;
-        this.posX = posX;
-        this.posY = posY;
         popUp = new TextField(fullString);
         popUp.setTranslateX(posX);
         popUp.setTranslateY(posY);
         popUp.setDisable(true);
-        popUp.setStyle("-fx-opacity: 1;");
+        popUp.setStyle("-fx-opacity: 0.6;");
     }
 
     TextField getPopUp() {
